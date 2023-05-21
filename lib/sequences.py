cb04461b6691e3cf4b3c56c80d8916a7fed8c428
waltersimekha <waltersimekha@gmail.com>
2023-05-21 15:59:31 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fibonacci_sequence = []
+
+    if length == 0:
+        print(fibonacci_sequence)
+    elif length >= 1:
+        fibonacci_sequence = [0, 1]
+        print(fibonacci_sequence[0])
+
+        if length >= 2:
+            print(fibonacci_sequence[1])
+
+            while len(fibonacci_sequence) < length:
+                next_num = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+                fibonacci_sequence.append(next_num)
+                print(next_num)
+
     pass
